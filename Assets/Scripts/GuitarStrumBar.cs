6b41ab2d0b81dfd6291d0bf854cb98b0ba6d7cb0
Ivan Felix <ivaneduardo68@gmail.com>
2020-11-25 14:46:44 -0500

    feat(GuitarStrumBar): add guitar strum movement script

---
+++ Assets/Scripts/GuitarStrumBar.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuitarStrumBar : MonoBehaviour
+{
+    [SerializeField] Transform targetRotation;
+    [SerializeField] bool pressed = true;
+    [SerializeField] float strumVelocity = 50f;
+    float timeCount = 0.0f;
+    GameObject startingObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingObj = new GameObject("StrumHelper");
+        Instantiate(startingObj);
+        startingObj.transform.position = transform.position;
+        startingObj.transform.rotation = transform.rotation;
+    }
+
+    void Update()
+    {
+        float step = strumVelocity * Time.deltaTime;
+        if(pressed)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation.rotation, step);
+            float diffInRotation = Mathf.Abs(transform.rotation.x - targetRotation.rotation.x);
+            print(diffInRotation);
+            //if(transform.rotation == targetRotation.rotation)
+            if(transform.rotation.x == targetRotation.rotation.x)
+            {
+                print("Match");
+                pressed = false;
+            }
+        } 
+        else
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, startingObj.transform.rotation, step);
+        }
+    }
+}
